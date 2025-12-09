7ee658b01fa02b9370fdd488652c7cd8df2b59d6
Anibal R. João <anibal.Joao@unizambeze.ac.mz>
2025-12-09 10:48:17 +0200

    Form validation for Betting

--- app/src/main/java/com/anibalofice/loteria/MainActivity.kt
+++ app/src/main/java/com/anibalofice/loteria/MainActivity.kt
@@ -7,13 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -21,14 +24,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -46,6 +54,8 @@ import com.anibalofice.loteria.components.LotItenType
 import com.anibalofice.loteria.components.LotNumberTextField
 import com.anibalofice.loteria.ui.theme.Green
 import com.anibalofice.loteria.ui.theme.LoteriaTheme
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -118,6 +128,9 @@ fun FormScreen(){
     ){innerPadding ->
         var qtdNumbers by remember { mutableStateOf("") }
         var qtdBets by remember {mutableStateOf("")}
+        var result by remember { mutableStateOf("") }
+        val snackBarHostState by remember { mutableStateOf(SnackbarHostState()) }
+        val scope = rememberCoroutineScope()
 
         Column(
             modifier = Modifier
@@ -161,16 +174,54 @@ fun FormScreen(){
             }
 
             OutlinedButton(
-                onClick = {}
+                enabled = qtdNumbers.isNotEmpty() && qtdBets.isNotEmpty(),
+                onClick = {
+                    if (qtdBets.toInt() < 1 || qtdBets.toInt() > 10){
+                        scope.launch {
+                            snackBarHostState.showSnackbar("Máximo de caracter permitido: 10")
+                        }
+                    }else if (qtdNumbers.toInt() < 1 || qtdNumbers.toInt() > 15){
+                        scope.launch {
+                            snackBarHostState.showSnackbar("Números devem ser de 6 á 15")
+                        }
+                    }else{
+                       result = ""
+                        for (i in 0..qtdBets.toInt()){
+                            result += "[$i] => ${numberGenerator(qtdNumbers)} \n\n"
+                        }
+                    }
+                }
             ) {
-                Text(
-                    text = stringResource(R.string.bets_generated)
-                )
+                Text(text = stringResource(R.string.bets_generated))
             }
 
+            Text(text = result)
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            SnackbarHost(
+                hostState = snackBarHostState,
+            )
         }
 
     }
+}
+
+private fun numberGenerator(qtd:String):String{
+    val numbers: MutableSet<Int> = mutableSetOf()
+
+    while (true){
+        val n = Random.nextInt(60)
+        numbers.add(n+1)
+        if(numbers.size == qtd.toInt()){
+            break
+        }
+    }
+    return numbers.joinToString(" - ")
 }
 
 private fun validatedInput(input:String):String{
